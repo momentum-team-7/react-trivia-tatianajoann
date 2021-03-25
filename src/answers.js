36832431273c26c5f4abc41c9d8223869107e444
Tatiana Perez <tjperez@ncsu.edu>
2021-03-25 16:42:47 -0400

    displays message when right/wrong answer clicked

--- src/answers.js
+++ src/answers.js
@@ -7,19 +7,30 @@ function Answers(props) {
     const {questions, correctAnswer, incorrectAnswers} = props
     const [selectedAnswer, setSelectedAnswer] = useState(null)
 
+    console.log(selectedAnswer)
+    console.log(correctAnswer)
+
+    
+
     return (
-        
+        <div>
           <ul>
-            <li><button>{correctAnswer}</button></li>
+            <li><button onClick={(e) => setSelectedAnswer(true)}>{correctAnswer}</button></li>
             {incorrectAnswers.map((incorrectAnswer, index) => (
-              <li><button>{incorrectAnswer}</button></li>
+              <li><button onClick={(e) => setSelectedAnswer(false)}>{incorrectAnswer}</button></li>
             ))}
             </ul> 
+          <div className="right" style={selectedAnswer ? {} : { display: 'none' }}>
+              Correct!!
+          </div>
+          
+          <div className="wrong" style={selectedAnswer === false ? {} : { display: 'none' }}>
+              Incorrect!!! > : (
+          </div>
+          </div>
+          
     )
-
-
-
-
 }
+
 
 export default Answers 
