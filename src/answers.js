dce5be5d6fcc6613c01a1ec663349065d6bdf585
Tatiana Perez <tjperez@ncsu.edu>
2021-03-27 19:16:29 -0400

    shuffling answers

--- src/answers.js
+++ src/answers.js
@@ -1,29 +1,34 @@
 import { useState, useEffect } from 'react'
 import he from 'he'
+import lodash from 'lodash'
 
 function Answers(props) {
-    const {questions, correctAnswer, incorrectAnswers} = props
+    const {questions, correctAnswer, possibleAnswers} = props
     const [selectedAnswer, setSelectedAnswer] = useState(null)
-
-    console.log(selectedAnswer)
-    console.log(correctAnswer)
-
-    
+    console.log('pos', possibleAnswers)
+    console.log('cor', correctAnswer)
+    // console.log(selectedAnswer)
+    // console.log(correctAnswer)
 
     return (
         <div>
           <ul>
-            <li><button onClick={(e) => setSelectedAnswer(true)}>{he.decode(correctAnswer)}</button></li>
+
+            {possibleAnswers.map((possibleAnswer, index) => (
+              <li>
+              <button onClick={() => correctAnswer === possibleAnswer ? setSelectedAnswer(true) : setSelectedAnswer(false)}>{he.decode(possibleAnswer)}</button>
+            </li>))}
+            {/* <li><button onClick={(e) => setSelectedAnswer(true)}>{he.decode(correctAnswer)}</button></li>
             {incorrectAnswers.map((incorrectAnswer, index) => (
               <li><button onClick={(e) => setSelectedAnswer(false)}>{he.decode(incorrectAnswer)}</button></li>
-            ))}
+            ))} */}
             </ul> 
           <div className="right" style={selectedAnswer ? {} : { display: 'none' }}>
               Correct!!
           </div>
           
           <div className="wrong" style={selectedAnswer === false ? {} : { display: 'none' }}>
-              Incorrect!!! > : (
+              Incorrect!!!
           </div>
           </div>
           
--- src/questions.js
+++ src/questions.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './app.css'
 import Answers from './answers'
 import he from 'he'
+import lodash from 'lodash'
 
 
 function Questions({ selectedCategory }) {
@@ -16,14 +17,14 @@ function Questions({ selectedCategory }) {
             
     })
     }, [])
-
+   
     return (
         <div className="App">
         <h1>{selectedCategory.name}</h1>
         <div>
             {questions.map((question, index) => (
             <div className="questions">{he.decode(question.question)}
-            <Answers questions={questions} correctAnswer={question.correct_answer} incorrectAnswers={question.incorrect_answers}/>
+            <Answers questions={questions} correctAnswer={question.correct_answer} possibleAnswers={lodash.shuffle(question.incorrect_answers.concat(question.correct_answer))}/>
            
             </div>
             ))}
